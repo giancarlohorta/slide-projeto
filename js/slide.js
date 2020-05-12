4ddf6c178da17dbee19ca0aefb3708a9525dbe2a
Giancarlo Horta <giancarlohorta@gmail.com>
2020-05-12 17:29:06 -0300

    Distancias atualizadas

--- js/slide.js
+++ js/slide.js
@@ -2,21 +2,36 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.dist = {
+      finalPosition: 0,
+      startX: 0,
+      moviment: 0,
+    };
+  }
+
+  moveSlide(distX) {
+    this.dist.movePostion = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
+
+  upDatePosition(clientX) {
+    this.dist.moviment = (this.dist.startX - clientX) * 1.6;
+    return this.dist.finalPosition - this.dist.moviment;
   }
 
   onStart(event) {
     event.preventDefault();
-    console.log("clicou");
+    this.dist.startX = event.clientX;
     this.wrapper.addEventListener("mousemove", this.onMove);
   }
 
   onEnd(event) {
-    console.log("deslicou");
+    this.dist.finalPosition = this.dist.movePostion;
     this.wrapper.removeEventListener("mousemove", this.onMove);
   }
   onMove(event) {
-    event.preventDefault();
-    console.log("moveu");
+    const finalPosition = this.upDatePosition(event.clientX);
+    this.moveSlide(finalPosition);
   }
   addSlideEvent() {
     this.wrapper.addEventListener("mousedown", this.onStart);
