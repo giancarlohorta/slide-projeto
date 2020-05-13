a001f6603a1028efbb3e7276decccc90a97ec3be
Giancarlo Horta <giancarlohorta@gmail.com>
2020-05-13 17:25:25 -0300

    adicionada paginação por slide

--- js/script.js
+++ js/script.js
@@ -3,6 +3,5 @@ import { SlideNav } from "./slide.js";
 const slide = new SlideNav(".slide", ".slide-wrapper");
 slide.init();
 slide.addArrow(".prev", ".next");
+slide.addControl();
 console.log(slide);
-
-slide.changeSlide(1);
